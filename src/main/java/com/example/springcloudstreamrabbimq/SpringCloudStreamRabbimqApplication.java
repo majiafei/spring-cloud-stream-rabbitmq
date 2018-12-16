b1fec6fc5a0e3aba2b3dce0c65fa7d92ba0894c5
1571221458@qq.com <majiafei>
2018-12-16 19:45:36 +0800

    stream-rabbit的使用

---
+++ src/main/java/com/example/springcloudstreamrabbimq/SpringCloudStreamRabbimqApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcloudstreamrabbimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamRabbimqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamRabbimqApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/springcloudstreamrabbimq/SpringCloudStreamRabbimqApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springcloudstreamrabbimq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStreamRabbimqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
